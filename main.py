0c3182b75edcd24b77828acba5611119be6bf6cc
Abhishek Kumar <abhishek.avanti@gmail.com>
2023-08-15 23:19:49 +0530

    Robo Realm 1.0

---
+++ main.py
@@ -0,0 +1,77 @@
+import subprocess
+
+if __name__ == '__main__':
+
+    print("Welcome to the RoboRealm 1.0 on Windows, Created by Abhishek.")
+    print("To end this program please enter q.\n")
+
+    powershell_command = fr'''
+    Add-Type -AssemblyName System.Speech
+    $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speechSynthesizer.Speak("Welcome to the RoboRealm 1.0 on Windows , Created by Abhishek.")
+    '''
+    subprocess.run(["powershell", "-Command", powershell_command])
+
+    while True:
+
+        # Take user-1 input
+        user_1_input = input("User-1: ")
+
+        if user_1_input == 'q':
+            print(
+                "Good Bye! -- RoboRealm is shutting down now. Please initiate the program again if you want to enter into RoboRealm.")
+
+            powershell_command = fr'''
+            Add-Type -AssemblyName System.Speech
+            $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+            $speechSynthesizer.Speak("Good Bye! -- RoboRealm is shutting down now. Please initiate the program again if you want to enter into RoboRealm.")
+            '''
+            subprocess.run(["powershell", "-Command", powershell_command])
+
+            break
+
+        # converting user-1 input as speech using Powershell command --Speak function
+        powershell_command = fr'''
+        Add-Type -AssemblyName System.Speech
+        $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+        $speechSynthesizer.Speak("{user_1_input}")
+        '''
+
+        # executing Powershell command for user-1
+        subprocess.run(["powershell", "-Command", powershell_command])
+
+        # Take user-2 input
+        user_2_input = input("User-2: ")
+
+        if user_2_input == 'q':
+            print(
+                "Good Bye! -- RoboRealm is shutting down now. Please initiate the program again if you want to enter into RoboRealm.")
+
+            powershell_command = fr'''
+            Add-Type -AssemblyName System.Speech
+            $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+            $speechSynthesizer.Speak("Good Bye! -- RoboRealm is shutting down now. Please initiate the program again if you want to enter into RoboRealm.")
+            '''
+            subprocess.run(["powershell", "-Command", powershell_command])
+
+            break
+
+        # converting user-1 input as speech using Powershell command --Speak function
+        powershell_command = fr'''
+        Add-Type -AssemblyName System.Speech
+        $speechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+        $speechSynthesizer.Speak("{user_2_input}")
+        '''
+
+        # executing Powershell command for user-1
+        subprocess.run(["powershell", "-Command", powershell_command])
+
+
+
+
+
+
+
+
+
+
